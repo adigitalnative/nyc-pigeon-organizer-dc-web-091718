dd55560bcd13adc1bc36018a3e06e8158562589b
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 14:27:12 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  
+  data.each do |attribute, attribute_details|
+    attribute_details.each do |detail, names|
+      names.each do |name|
+        if pigeons.keys.include?(name)
+          if pigeons[name][attribute]
+            pigeons[name][attribute] << detail
+          else
+            pigeons[name][attribute] = [detail]
+        else
+          pigeons[name][attribute] = [detail]
+        end
+      end
+    end
+  end
+  
+  pigeons
 end
